316f0657b2e5c46515499b2111bbf44c42344aea
ThanhTuan208 <161844395+ThanhTuan208@users.noreply.github.com>
2024-03-12 14:23:33 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,137 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bai_5_CTDL_GT
+namespace CTDL_GT
 {
-
-    struct Sach
-    {
-        public string MaSach;
-        public string TuaSach;
-        public int NamXB;
-        public int GiaBan;
-
-    }
     internal class Program
-    {      
+    {
         static void Main(string[] args)
         {
-           
-            Sach[] arr = new Sach[1]; 
-            nhapMangSach(arr);
-            xuatMangSach(arr);
-            cau2b(arr);
-            arr = cau2c(arr);
-            xuatMangSach(arr);
+            Console.Write("num = ");
+            int num = int.Parse(Console.ReadLine());
 
+            int[] arr = new int[num];
+            nhapMang(arr);
+            xuatMang(arr);
+            timTuanTu(arr, num);
+            nhiPhan(arr,num);
         }
-        //bai5 cau 2c
-        static Sach[] cau2c(Sach[] arr)
+        static void nhiPhan(int[] arr, int key)
         {
-            Console.WriteLine("nhap ma sach can xoa: ");
-            string maSach = Console.ReadLine();
-            int viTri = timNhiPhan(arr, maSach);
+            int left = 0;
+            int right = arr.Length -1;
+            int count =0;
 
-            if (viTri == -1)
-            {
-                Console.WriteLine("ko tim thay sach de xoa");
-                return arr;
-
-            }
-            else
-            {
-                for (int i = viTri; i < arr.Length - 1; i++)
-                {
-                    arr[i] = arr[i + 1];
-                }
-                Array.Resize(ref arr, arr.Length - 1);
-                return arr; 
-            }
-            
-        }
-
-        //bai 5 cau 2b
-        static void cau2b(Sach[] arr)
-        {
-            Console.WriteLine("nhap tua sach can tim de sua gia ban: ");
-            string tuaSach = Console.ReadLine();
-            int viTri = timTuanTu(arr, tuaSach);
-            
-            if (viTri == -1)
-            {
-                Console.WriteLine("ko tim thay de sua");
-
-            }
-            else
-            {
-                Console.WriteLine("nhap gia tri gia ban moi: ");
-                int.TryParse(Console.ReadLine(), out arr[viTri].GiaBan);
-                Console.WriteLine("gia ban moi la: ");
-                xuatMangSach(arr);
-            }
-        }
-        // ham nhi phan theo ma sach
-        static int timNhiPhan(Sach[] arr, string maSach)
-        {
-            int left = 0, right = arr.Length - 1, mid;
+            int mid;
             while (left <= right)
             {
                 mid = (left + right) / 2;
-                if (maSach == arr[mid].MaSach)
+                count++;
+                Console.WriteLine($"lan {count}: key = {key} nam trong doan [{left}...{right}] \\ mid {mid}");
+                if (arr[mid] == key)
                 {
-                    return mid;
+                    break;
                 }
-                else if (String.Compare(maSach, arr[mid].MaSach) == -1)
+                else if (key < arr[mid])
                 {
                     right = mid - 1;
                 }
                 else
                 {
+
                     left = mid + 1;
-                }     
+                }
+                Console.WriteLine($"so la chia doi la: {count} ");
             }
-            return -1;
 
         }
-        // ham tim tuan tu theo ma sach
-        static int timTuanTu(Sach[] arr, string tuaSach)
+        static int timTuanTu(int[] arr, int key)
         {
-            for(int i =0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                if (arr[i].TuaSach == tuaSach)
+                if (arr[i] == key)
                 {
                     return i;
                 }
             }
             return -1;
         }
-
-        // ham xuat mang sach
-        static void xuatMangSach(Sach[] arr)
-        {
-            Console.WriteLine($"{ "MaSach", -10}{"TuaSach", -20} {"NamXB", -10}{"GiaBan"}");
-           for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine($"{arr[i].MaSach, -10}{arr[i].TuaSach, -20} {arr[i].NamXB, -10}{arr[i].GiaBan}");
-            }
-        }
-        //ham nhap mang sach
-        static void nhapMangSach(Sach[] arr)
+        static void xuatMang(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine($"nhap thong tin cho sach thu {i+1}: ");
-                Console.Write("nhap ma sach: ");
-                arr[i].MaSach = Console.ReadLine();
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+        static void nhapMang(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"array[{i}] = ");
+                int.TryParse(Console.ReadLine(), out arr[i]);
 
-                Console.Write("tua sach: ");
-                arr[i].TuaSach = Console.ReadLine();
-
-                Console.Write("Nhap namXB: ");
-                int.TryParse(Console.ReadLine(), out arr[i].NamXB);
-
-                Console.Write("nhap GiaBan: ");
-                int.TryParse(Console.ReadLine(), out arr[i].GiaBan);
             }
         }
     }
